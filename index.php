167b3ef904c3d2d5138b4cc9ce99a66d34407433
TheGivingShree <danielxustem@gmail.com>
2015-03-30 17:38:29 -0400

    Add Jquery and set up index.php
    more setup

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-            echo "hello";
-            echo "dawg";
-            echo "i can believe that its butter"
-        ?>
-    </body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, 
+➥initial-scale=1">
+
+    <title>My First Bootstrap Application</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    
+  </head>
+  <body>
+        <h1>Hello World!</h1>
+        
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+  </body>
 </html>
